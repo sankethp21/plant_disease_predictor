23b40eaf1c940b76387c3a2f9394bbe53c57da18
Sanketh P <137313492+sankethp21@users.noreply.github.com>
2024-04-23 09:26:09 +0530

    just to see if it works.....

---
+++ main.dart
@@ -0,0 +1,109 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_v2/tflite_v2.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ImagePickerDemo(),
+    );
+  }
+}
+
+class ImagePickerDemo extends StatefulWidget {
+  @override
+  _ImagePickerDemoState createState() => _ImagePickerDemoState();
+}
+
+class _ImagePickerDemoState extends State<ImagePickerDemo> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+  File? file;
+  var _recognitions;
+  var result = "";
+  var label = "";
+  var conf = "";
+  @override
+  void initState() {
+    super.initState();
+    loadmodel().then((value) {
+      setState(() {});
+    });
+  }
+
+  loadmodel() async {
+    await Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+
+  Future<void> _pickImage() async {
+    try {
+      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      setState(() {
+        _image = image;
+        file = File(image!.path);
+      });
+      detectimage(file!);
+    } catch (e) {
+      print('Error picking image: $e');
+    }
+  }
+
+  Future detectimage(File image) async {
+    var recognitions = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 6,
+      threshold: 0.05,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _recognitions = recognitions;
+      result = recognitions.toString();
+      label = result.substring(52, result.indexOf('}'));
+      conf = result.substring(2, 20);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plant Disease Predictor'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_image != null)
+              Image.file(
+                File(_image!.path),
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
+              )
+            else
+              const Text('No image selected'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _pickImage,
+              child: const Text('Pick Image from Gallery'),
+            ),
+            const SizedBox(height: 20),
+            Text("Prediction: " + label,
+                style: const TextStyle(fontWeight: FontWeight.bold)),
+            Text(conf, style: const TextStyle(fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
